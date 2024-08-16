268b1653261b0c88e435a1d204b06c9bb58584bb
Raul David Muñoz Castiblanco <raul.munoz@co.sonda.com>
2024-08-16 13:14:59 -0500

    Inicio del proyecto

---
+++ Models/TurnosContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> opciones)
+        :base(opciones)
+        {
+        }
+
+        public DbSet<Especialidad> Especialidad {get; set; }
+    }
+}
