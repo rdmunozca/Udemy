f64b78ed16f6dc51c8872cf2ac6468fef1c76f81
Raul Munoz <rdmunoz2@gmail.com>
2024-08-21 18:02:02 -0500

    Se agrega Modelo y Controlador Paciente

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -10,5 +10,47 @@ namespace Turnos.Models
         }
 
         public DbSet<Especialidad> Especialidad {get; set; }
+
+        public DbSet<Paciente> Paciente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad =>
+            {
+                entidad.ToTable("Especialidad");
+                entidad.HasKey(e => e.IdEspecialidad);
+                entidad.Property(e => e.Descripcion)
+                .IsRequired()
+                .HasMaxLength(256)
+                .IsUnicode(false);
+            }
+            );
+            modelBuilder.Entity<Paciente>(entidad =>
+            {
+                entidad.ToTable("Paciente");
+                entidad.HasKey(p => p.IdPaciente);
+                entidad.Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+                entidad.Property(p => p.Apellido)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+                entidad.Property(p => p.Direccion)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+                entidad.Property(p => p.Telefono)
+                .IsRequired()
+                .HasMaxLength(10)
+                .IsUnicode(false);
+                entidad.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            });
+        }
+        
     }
 }
